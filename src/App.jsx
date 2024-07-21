838ec831e2ddb03ff76bb6fe5b7a505fde80e41b
Jesus Retana Artavia <jesus@Jesuss-MacBook-Air.local>
2024-07-21 14:36:24 -0600

    update with grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import '../src/style/style.css'
 import Navbar from './components/navbar'
+import Meme from './components/meme'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Navbar />
+    <Meme/>
     </>
   )
 }
--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -5,9 +5,20 @@ export default
 function Meme() {
     return(
         <section className="inputSection">
-            <form action="">
-            
-
+            <form action="" className="formSection">
+                <main className="mainMeme">
+                <section className="TextSection">
+                <label htmlFor="topText">Top Text</label>
+                <input placeholder="Text Here"  className="inputText" type="text" name="topText" id="topText" />
+                </section>
+                <section className="TextSection">
+                <label htmlFor="bottomText">Bottom Text</label>
+                <input placeholder="Text Here" className="inputText" type="text" name="bottomText" id="bottomText" />
+                </section>
+                </main>
+                <button className="fromBtn">
+                Get a new meme image  🖼
+                </button>
             </form>
         </section>
     )
