ec7577d96289dc036db6bd3b82ba731b2823037f
Jesus Retana Artavia <jesus@Jesuss-MacBook-Air.local>
2024-07-21 18:02:29 -0600

    get new img

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -1,12 +1,26 @@
 import "../style/Meme.css"
+import memesData from "../memesData"
+import { useState } from 'react';
 
 export default
 
 function Meme() {
+
+    const [memeImg, setImg] = useState('');
+
+    function buttonClickHadler() {
+        const memesArray = memesData.data.memes
+        const randomPosition = Math.floor(Math.random()*memesArray.length)
+        const imgURL = memesArray[randomPosition].url
+        setImg(imgURL)
+        return 
+    }
+
+    
+
     return(
         <section className="inputSection">
-            <form action="" className="formSection">
-                <main className="mainMeme">
+            <main className="mainMeme">
                 <section className="TextSection">
                 <label htmlFor="topText">Top Text</label>
                 <input placeholder="Text Here"  className="inputText" type="text" name="topText" id="topText" />
@@ -16,10 +30,14 @@ function Meme() {
                 <input placeholder="Text Here" className="inputText" type="text" name="bottomText" id="bottomText" />
                 </section>
                 </main>
-                <button className="fromBtn">
+                 <form action="" className="formSection"  onSubmit={e => e.preventDefault()}>
+                <button  onClick={buttonClickHadler} className="fromBtn">
                 Get a new meme image  🖼
                 </button>
             </form>
+            <img className="memeImg" src={memeImg} alt="" />
         </section>
     )
 }
+
+
